1767196a4bb38a51772c5a5ae77204eb53a58405
chuyfabian33 <chuyfabian33@gmail.com>
2025-11-30 11:23:04 -0600

    controladores/modelos/servicios

---
+++ rental-dvd-frontend/models/__init__.py
@@ -0,0 +1,9 @@
+"""
+Paquete de modelos de datos
+"""
+from .cliente import Cliente
+from .dvd import DVD
+from .staff import Staff
+from .renta import Renta
+
+__all__ = ['Cliente', 'DVD', 'Staff', 'Renta']
