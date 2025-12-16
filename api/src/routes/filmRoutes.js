b73fba078e650fdbe079d9c4431b09852dbb721c
Samuel Jafet Juárez Baliño <samuelj.b.190@gmail.com>
2025-12-15 22:57:43 -0600

    Documentación

--- api/src/routes/filmRoutes.js
+++ api/src/routes/filmRoutes.js
@@ -4,8 +4,11 @@ const filmController = require('../controllers/filmController');
 
 // Rutas de películas
 router.get('/', filmController.getAllFilms);
+// Ruta para buscar películas por título
 router.get('/search', filmController.searchFilmsByTitle);
+// Ruta para obtener películas por categoría
 router.get('/category/:category', filmController.getFilmsByCategory);
+// Ruta para obtener detalles de una película por ID
 router.get('/:id', filmController.getFilmById);
 
 module.exports = router;
